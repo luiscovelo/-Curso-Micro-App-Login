0a1f859e83eee70c0ccabbf90a01a37b39ea1d6c
Luis Felipe Covelo <covelo.luis@hotmail.com>
2022-01-17 07:20:20 -0300

    add button to home page

--- lib/app/page/login_page.dart
+++ lib/app/page/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:micro_commons_design_system/widgets/button/custom_button_widget.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,6 +10,12 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
+      ),
+      body: CustomButtonWidget(
+        text: 'goToHome',
+        onPressed: () {
+          navigatorKey.currentState!.pushNamed('/home');
+        },
       ),
     );
   }
